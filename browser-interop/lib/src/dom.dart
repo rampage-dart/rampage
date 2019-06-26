4d8be9fb9803630a1084de1473ae7950be4d77d5
Don <don.j.olmstead@gmail.com>
2019-06-25 21:15:26 -0700

    feat(interop): Add document.createElement

--- browser-interop/lib/src/dom.dart
+++ browser-interop/lib/src/dom.dart
@@ -9,12 +9,39 @@ library rampage_browser_interop.src.dom;
 import 'package:js/js.dart';
 
 //-----------------------------------------------------------
+// Elements
+//-----------------------------------------------------------
+
+/// [Element] is the most general base class from which all objects in a
+/// [Document] inherit.
+///
+/// It only has methods and properties common to all kinds of elements. More
+/// specific classes inherit from [Element]. For example, the [HtmlElement]
+/// interface is the base interface for HTML elements. Most functionality is
+/// specified further down the class hierarchy.
+@JS('Element')
+class Element {
+  /// The name of the tag for the given element
+  external String get tagName;
+}
+
+/// The [HtmlElement] interface represents any HTML element.
+@JS('HTMLElement')
+class HtmlElement extends Element {}
+
+/// The [UnknownElement] interface represents an invalid HTML element and
+/// derives from the [HtmlElement] interface, but without implementing any
+/// additional properties or methods.
+@JS('HTMLUnknownElement')
+class UnknownElement extends HtmlElement {}
+
+//-----------------------------------------------------------
 // Document
 //-----------------------------------------------------------
 
 /// The [Document] interface represents any web page loaded in the browser and
 /// serves as an entry point into the web page's content, which is the DOM tree.
-/// 
+///
 /// The DOM tree includes elements such as `<body>` and `<table>`, among many
 /// others. It provides functionality globally to the document, like how to
 /// obtain the page's URL and create new elements in the document.
@@ -25,6 +52,10 @@ class Document {
   /// 
   /// Possible values are `rtl`, right to left, and `ltr`, left to right.
   external String get dir;
+
+  /// Creates the HTML element specified by [tagName], or an [UnknownElement] if
+  /// [tagName] isn't recognized.
+  external Element createElement(String tagName);
 }
 
 //-----------------------------------------------------------
@@ -33,7 +64,7 @@ class Document {
 
 /// The [Window] interface represents a window containing a DOM document; the
 /// [Window.document] property points to the DOM document loaded in that window.
-/// 
+///
 /// The [Window] interface is home to a variety of functions, namespaces,
 /// objects, and constructors which are not necessarily directly associated with
 /// the concept of a user interface window. However, the Window interface is a
