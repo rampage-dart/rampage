863ab4c0d7f1823043428d2160078df8b6b9258e
Don <don.j.olmstead@gmail.com>
2019-06-25 22:25:18 -0700

    feat(interop): Add additional ParentNode methods

--- browser-interop/lib/src/node.dart
+++ browser-interop/lib/src/node.dart
@@ -66,6 +66,45 @@ class NodeList {
 /// all types of [Node] objects that can have children.
 @JS()
 mixin ParentNode {
+  /// Returns the number of children of this [ParentNode] which are [Element]s.
+  external int get childElementCount;
+
+  /// Returns the first [Node] which is both a child of this [ParentNode] and is
+  /// is also an [Element], or `null` if there is none.
+  external Element get firstElementChild;
+
+  /// Returns the last [Node] which is both a child of this [ParentNode] and is
+  /// also an [Element], or `null` if there is none.
+  external Element get lastElementChild;
+
+  /// Inserts a set of [Node]s after the last child of the [ParentNode].
+  external void append(
+    Node node0, [
+    Node node1,
+    Node node2,
+    Node node3,
+    Node node4,
+    Node node5,
+    Node node6,
+    Node node7,
+    Node node8,
+    Node node9,
+  ]);
+
+  /// Inserts a set of [Node]s before the first child of the [ParentNode].
+  external void prepend(
+    Node node0, [
+    Node node1,
+    Node node2,
+    Node node3,
+    Node node4,
+    Node node5,
+    Node node6,
+    Node node7,
+    Node node8,
+    Node node9,
+  ]);
+
   /// Returns the first [Element] with the current element as root that matches
   /// the specified group of [selectors].
   external Element querySelector(String selectors);
