04998f7c309093c68372da79cd3109f6d6dd9bee
Don <don.j.olmstead@gmail.com>
2019-11-11 23:24:05 -0800

    feat(interop): Add CssStyleSheet

--- browser-interop/lib/src/style_sheet.dart
+++ browser-interop/lib/src/style_sheet.dart
@@ -20,4 +20,69 @@ import 'js_object.dart';
 /// CSS style sheets will further implement the more specialized [CssStyleSheet]
 /// interface.
 @JS('StyleSheet')
-class StyleSheet extends JsObject {}
+class StyleSheet extends JsObject {
+  /// Whether the current stylesheet has been applied or not.
+  external bool get disabled;
+
+  /// The advisory title of the current style sheet.
+  external String get title;
+}
+
+//-----------------------------------------------------------
+// CssStyleSheet
+//-----------------------------------------------------------
+
+/// The [CssStyleSheet] interface represents a single CSS stylesheet, and lets
+/// you inspect and modify the list of rules contained in the stylesheet.
+@JS('CSSStyleSheet')
+class CssStyleSheet extends StyleSheet {
+  /// Creates a new [CssStyleSheet] from the [options].
+  ///
+  /// Constructor is an extension from the
+  /// [Constructable Stylesheet Objects](https://wicg.github.io/construct-stylesheets)
+  /// API.
+  external factory CssStyleSheet([CssStyleSheetInit options]);
+
+  /// Deletes the rule at the specified index into the stylesheet's rule list.
+  external void deleteRule(int index);
+
+  /// Inserts a new rule at the specified position in the stylesheet, given the
+  /// textual representation of the rule.
+  external void insertRule(String text, [int index]);
+
+  /// Replace the contents of the style sheet with the [text].
+  ///
+  /// Method is an extension from the
+  /// [Constructable Stylesheet Objects](https://wicg.github.io/construct-stylesheets)
+  /// API.
+  external void replaceSync(String text);
+}
+
+/// Represents options that represent characteristics of a [CssStyleSheet].
+@JS()
+@anonymous
+class CssStyleSheetInit {
+  /// Creates the [CssStyleSheetInit] options.
+  external factory CssStyleSheetInit({
+    String media = '',
+    String title = '',
+    bool alternate = false,
+    bool disabled = false,
+  });
+
+  /// The intended destination medium for style information.
+  external String get media;
+  external set media(String value);
+
+  /// The advisory title for the style sheet.
+  external String get title;
+  external set title(String value);
+
+  /// Whether the style sheet is not the default.
+  external bool get alternate;
+  external set alternate(bool value);
+
+  /// Whether the style sheet will be applied or not.
+  external bool get disabled;
+  external set disabled(bool value);
+}
