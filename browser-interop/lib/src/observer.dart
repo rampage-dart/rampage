35f33c79f0ca8e7767cfe39dab5e14eb846a64da
Don <don.j.olmstead@gmail.com>
2019-11-24 14:45:38 -0800

    feat(interop): Add IntersectionObserver

--- browser-interop/lib/src/observer.dart
+++ browser-interop/lib/src/observer.dart
@@ -13,6 +13,118 @@ import 'element.dart';
 import 'js_object.dart';
 
 //------------------------------------------------------------------------------
+// IntersectionObserver
+//------------------------------------------------------------------------------
+
+/// Provides a way to asynchronously observe changes in the intersection of a
+/// target [Element] with an ancestor [Element] or with a top-level [Document]'s
+/// viewport.
+@JS('IntersectionObserver')
+class IntersectionObserver extends JsObject {
+  /// Creates a new [IntersectionObserver] which will execute the specified
+  /// [callback] function when it detects that a target [Element]'s visibility
+  /// has crossed one or more thresholds.
+  external factory IntersectionObserver(IntersectionObserverCallback callback);
+
+  /// A specific ancestor of the target [Element] being observed.
+  ///
+  /// If no value was passed to the constructor or this is `null`, the
+  /// top-level [Document]'s viewport is used.
+  external Element get root;
+
+  /// An offset rectangle applied to the [root]'s bounding box when calculating
+  /// intersections, effectively shrinking or growing the [root] for calculation
+  /// purposes.
+  external String get rootMargin;
+
+  /// A list of thresholds, sorted in increasing numeric order, where each
+  /// threshold is a ratio of intersection area to bounding box area of an
+  /// observed target.
+  external List<double> get thresholds;
+
+  /// Tells the [IntersectionObserver] a target [element] to observe.
+  external void observe(Element element);
+
+  /// Tells the [IntersectionObserver] to stop observing a particular target
+  /// [element].
+  external void unobserve(Element element);
+
+  /// Stops the [IntersectionObserver] from observing any target.
+  external void disconnect();
+}
+
+/// A function which is called when the percentage of the target [Element] is
+/// visible crosses a threshold.
+typedef IntersectionObserverCallback = Function(
+  List<IntersectionObserverEntry> entries,
+  IntersectionObserver observer,
+);
+
+/// Describes the intersection between the target [Element] and its root
+/// container at a specific moment of transition.
+class IntersectionObserverEntry {
+  /// The bounds rectangle of the [target] element.
+  external ImmutableRectangle get boundingClientRect;
+
+  /// The ratio of the [intersectionRect] to the [boundingClientRect].
+  external double get intersectionRatio;
+
+  /// Represents the [target]'s visible area.
+  external ImmutableRectangle get intersectionRect;
+
+  /// Returns `true` if the [target] element intersects with the intersection
+  /// observer's root.
+  ///
+  /// If this is `true`, then, the [IntersectionObserverEntry] describes a
+  /// transition into a state of intersection; if it's `false`, then you know
+  /// the transition is from intersecting to not-intersecting.
+  external bool get isIntersecting;
+
+  /// The bounds rectangle for intersection observer's root.
+  external ImmutableRectangle get rootBounds;
+
+  /// The Element whose intersection with the root changed.
+  external Element get target;
+
+  /// The time at which the intersection was recorded, relative to the
+  /// [IntersectionObserver]'s time origin.
+  external double get time;
+}
+
+/// Object allowing you to set options for the [IntersectionObserver].
+@JS()
+@anonymous
+class IntesectionObserverOptions {
+  /// Creates the [IntesectionObserverOptions].
+  external factory IntesectionObserverOptions({
+    Element root,
+    String rootMargin = '0px',
+    List<double> threshold = const [0.0],
+  });
+
+  /// An [Element] which is an ancestor of the intended target, whose bounding
+  /// rectangle will be considered the viewport.
+  ///
+  /// Any part of the target not visible in the visible area of the root is not
+  /// considered visible.
+  external Element get root;
+  external set root(Element value);
+
+  /// A string which specifies a set of offsets to add to the root's bounding
+  /// box when calculating intersections, effectively shrinking or growing the
+  /// root for calculation purposes.
+  external String get rootMargin;
+
+  /// A list of numbers between 0.0 and 1.0, specifying a ratio of intersection
+  /// area to total bounding box area for the observed target.
+  ///
+  /// A value of 0.0 means that even a single visible pixel counts as the target
+  /// being visible.
+  external List<double> get threshold;
+  external set threshold(List<double> value);
+}
+
+//------------------------------------------------------------------------------
 // ResizeObserver
 //------------------------------------------------------------------------------
 
@@ -61,7 +173,7 @@ class ResizeObserverSize {
   external double get blockSize;
 }
 
-/// Object allowing you to set options for the observation.
+/// Object allowing you to set options for the [ResizeObserver].
 @JS()
 @anonymous
 class ResizeObserverOptions {
