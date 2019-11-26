d5e8af86c7fa0b0bdecb712507c931295f2eed85
Don <don.j.olmstead@gmail.com>
2019-11-25 21:38:22 -0800

    feat(html): Expose CssStyleSheet constructor

--- html/lib/src/style_sheet.dart
+++ html/lib/src/style_sheet.dart
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
+import 'package:rampage_html/browser/html.dart' as impl;
+
 //-----------------------------------------------------------
 // StyleSheet
 //-----------------------------------------------------------
@@ -27,6 +29,36 @@ abstract class StyleSheet {
 /// The [CssStyleSheet] interface represents a single CSS stylesheet, and lets
 /// you inspect and modify the list of rules contained in the stylesheet.
 abstract class CssStyleSheet implements StyleSheet {
+  /// Creates a new [CssStyleSheet] from the [options].
+  factory CssStyleSheet([CssStyleSheetInit options]) = impl.CssStyleSheetImpl;
+
   /// Replace the contents of the style sheet with the [text].
   void replaceSync(String text);
+}
+
+/// Represents options that represent characteristics of a [CssStyleSheet].
+abstract class CssStyleSheetInit {
+  /// Creates the [CssStyleSheetInit] options.
+  factory CssStyleSheetInit({
+    String media,
+    String title,
+    bool alternate,
+    bool disabled,
+  }) = impl.CssStyleSheetInitImpl;
+
+  /// The intended destination medium for style information.
+  String get media;
+  set media(String value);
+
+  /// The advisory title for the style sheet.
+  String get title;
+  set title(String value);
+
+  /// Whether the style sheet is not the default.
+  bool get alternate;
+  set alternate(bool value);
+
+  /// Whether the style sheet will be applied or not.
+  bool get disabled;
+  set disabled(bool value);
 }
