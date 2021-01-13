7f6a0293766ba8489d3763db481b53fd3760d481
Don <don.j.olmstead@gmail.com>
2021-01-13 14:08:06 -0800

    feat(html): Add CustomElement definition
    Implement the interface for a CustomElement. At this time only HtmlElement can be subclassed.

---
+++ html/lib/browser/src/custom_element.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+import 'dart:js';
+
+import 'package:rampage_html/html.dart';
+
+import 'html_element.dart';
+
+/// Browser implementation of [CustomElement].
+class CustomElementImpl extends HtmlElementImpl implements CustomElement {
+  /// Create an instance of [CustomElementImpl] from the [jsObject].
+  CustomElementImpl.fromJsObject(JsObject jsObject)
+      : super.fromJsObject(jsObject);
+
+  @override
+  void connected() {}
+
+  @override
+  void disconnected() {}
+
+  @override
+  void attributeChanged(String name, String? oldValue, String? newValue) {}
+}
