89fdc38e5525f18fb9863ab19d1e7d4f3320d2f9
Don <don.j.olmstead@gmail.com>
2021-01-13 12:47:03 -0800

    feat(html): Add stub for creating a JsObject representing an Element
    This function will be used by constructors in the Element hierarchy.

---
+++ html/lib/browser/src/element_factory.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+import 'dart:js';
+
+/// Calls `document.createElement` to create a [JsObject] that represents a
+/// `HTMLElement`.
+JsObject createElementJsObject(String tag) =>
+    throw UnimplementedError('createElementJsObject not implemented');
