1d96bd201ffc4dad405d097247bc0be2957708dc
Don <don.j.olmstead@gmail.com>
2021-01-13 08:58:47 -0800

    refactor(html): Remove internal interop library
    All the files are being rewritten so just remove for the moment.

--- html/lib/browser/html.dart
+++ html/lib/browser/html.dart
@@ -1,20 +1,4 @@
-// Copyright (c) 2019 the Rampage Project Authors.
+// Copyright (c) 2021 the Rampage Project Authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
-
-export 'src/child_node.dart';
-export 'src/css_style_sheet.dart';
-export 'src/custom_element.dart';
-export 'src/custom_event.dart';
-export 'src/document.dart';
-export 'src/element.dart';
-export 'src/event.dart';
-export 'src/event_target.dart';
-export 'src/geometry.dart';
-export 'src/node.dart';
-export 'src/observer.dart';
-export 'src/parent_node.dart';
-export 'src/slotable.dart';
-export 'src/style_sheet.dart';
-export 'src/window.dart';
