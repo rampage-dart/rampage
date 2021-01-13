d74f086e9fb59cbb1db9678d768427361286de70
Don <don.j.olmstead@gmail.com>
2021-01-13 15:53:06 -0800

    feat(html): Add ResizeObserver definitions
    Create a stub for the browser implementation and its IDL file.

--- html/lib/browser/html.dart
+++ html/lib/browser/html.dart
@@ -20,6 +20,7 @@ export 'src/non_document_type_child_node.dart';
 export 'src/non_element_parent_node.dart';
 export 'src/parent_node.dart';
 export 'src/rectangle.dart';
+export 'src/resize_observer.dart';
 export 'src/shadow_root.dart';
 export 'src/slot_element.dart';
 export 'src/slotable.dart';
