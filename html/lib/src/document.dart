b1d29e74dd6355e59ad98eac5c1f0ce2c379b37d
Don <don.j.olmstead@gmail.com>
2021-01-13 11:39:03 -0800

    feat(html): Add Document definition
    Create a stub for the browser implementation and its IDL file.

--- html/lib/src/document.dart
+++ html/lib/src/document.dart
@@ -1,21 +1,20 @@
-// Copyright (c) 2019 the Rampage Project Authors.
+// Copyright (c) 2021 the Rampage Project Authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
+import 'document_or_shadow_root.dart';
 import 'element.dart';
 import 'node.dart';
 import 'non_element_parent_node.dart';
 import 'parent_node.dart';
 
-//-----------------------------------------------------------
-// Document
-//-----------------------------------------------------------
-
 /// The [Document] interface represents any web page loaded in the browser and
 /// serves as an entry point into the web page's content, which is the DOM tree.
-abstract class Document implements Node, ParentNode, NonElementParentNode {
-  /// Represents the `<body>` of the current [Document], or `null` if no such
+abstract class Document
+    implements Node, ParentNode, NonElementParentNode, DocumentOrShadowRoot {
+  /// Represents the body of the current [Document], or `null` if no such
   /// [Element] exists.
-  BodyElement get body;
+  HtmlElement? get body;
+  set body(HtmlElement? value);
 }
