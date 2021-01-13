db9d6ef1e31e7370a4b01f3410397d75d0710644
Don <don.j.olmstead@gmail.com>
2021-01-13 11:07:53 -0800

    feat(html): Add DocumentOrShadowRoot definition
    Create a stub for the browser implementation and its IDL file.

--- html/lib/browser/html.dart
+++ html/lib/browser/html.dart
@@ -5,6 +5,7 @@
 
 export 'src/child_node.dart';
 export 'src/custom_event.dart';
+export 'src/document_or_shadow_root.dart';
 export 'src/event.dart';
 export 'src/event_target.dart';
 export 'src/node.dart';
