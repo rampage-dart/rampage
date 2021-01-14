53d27422083bd5f680c929adccb0605db9a9f3fa
Don <don.j.olmstead@gmail.com>
2021-01-13 16:41:21 -0800

    feat(html): Implement createElement
    Add functions to convert from a JsObject into a Dart Element.
    
    Support tagName on an Element. Change all tag names to upper case since that's returned.

--- html/lib/browser/src/element_factory.dart
+++ html/lib/browser/src/element_factory.dart
@@ -7,11 +7,49 @@ import 'dart:js';
 
 import 'package:rampage_html/html.dart';
 
+import 'body_element.dart';
+import 'div_element.dart';
+import 'element_tag.dart';
+import 'slot_element.dart';
+import 'template_element.dart';
+import 'wrapper.dart';
+
 /// Creates an [Element] from the given [tagName].
 Element createElement(String tag) =>
-    throw UnimplementedError('createElement not implemented');
+    elementFromJsObject(createElementJsObject(tag));
 
 /// Calls `document.createElement` to create a [JsObject] that represents a
 /// `HTMLElement`.
-JsObject createElementJsObject(String tag) =>
-    throw UnimplementedError('createElementJsObject not implemented');
+JsObject createElementJsObject(String tag) {
+  final document = JsObject.fromBrowserObject(context['document'] as Object);
+  final jsElement =
+      document.callMethod('createElement', <String>[tag]) as Object;
+
+  return JsObject.fromBrowserObject(jsElement);
+}
+
+/// Creates an instance of [T] from the [jsObject].
+///
+/// Uses the [interop.Element.tagName] to determine the [Element] to construct.
+T elementFromJsObject<T extends Element>(JsObject jsObject) {
+  final tag = jsObject.tagName;
+  switch (tag) {
+    case ElementTag.div:
+      return DivElementImpl.fromJsObject(jsObject) as T;
+    case ElementTag.slot:
+      return SlotElementImpl.fromJsObject(jsObject) as T;
+    case ElementTag.template:
+      return TemplateElementImpl.fromJsObject(jsObject) as T;
+    case ElementTag.body:
+      return BodyElementImpl.fromJsObject(jsObject) as T;
+  }
+
+  throw UnsupportedError('$tag not supported');
+}
+
+/// Creates an instance of [T] from the [jsObject].
+///
+/// This function should be used when its unclear if the [jsObject] has already
+/// has already been wrapped.
+T safeElementFromJsObject<T extends Element>(JsObject jsObject) =>
+    (jsObject.dartObject ?? elementFromJsObject<T>(jsObject)) as T;
--- html/lib/browser/src/element_tag.dart
+++ html/lib/browser/src/element_tag.dart
@@ -8,14 +8,16 @@ import 'dart:js';
 /// Extension for getting the `tagName` from a [JsObject] representing an
 /// [Element].
 extension ElementTag on JsObject {
+  // Element.tagName always returns upper case tags
+
   /// `<body>`
-  static const String body = 'body';
+  static const String body = 'BODY';
   /// `<div>`
-  static const String div = 'div';
+  static const String div = 'DIV';
   /// `<slot>`
-  static const String slot = 'slot';
+  static const String slot = 'SLOT';
   /// `<template>`
-  static const String template = 'template';
+  static const String template = 'TEMPLATE';
 
   /// The tag name of the DOM `Element`.
   String get tagName => this['tagName'] as String;
