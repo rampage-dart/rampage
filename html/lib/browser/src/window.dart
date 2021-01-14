a0a681dc5671b9ae373e4cbc9b4f8e349b4c2c7c
Don <don.j.olmstead@gmail.com>
2021-01-13 16:15:27 -0800

    feat(html): Add Window definition
    Create a stub for the browser implementation and its IDL file.

---
+++ html/lib/browser/src/window.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+import 'dart:js';
+
+import 'package:rampage_html/html.dart';
+
+import 'wrapper.dart';
+
+/// Browser implementation of [Window].
+class WindowImpl extends DartJsWrapper implements Window {
+  /// Create an instance of [WindowImpl] from the [jsObject].
+  WindowImpl.fromJsObject(JsObject jsObject) : super.fromJsObject(jsObject);
+
+  /// Create an instance of [WindowImpl] from the [jsObject].
+  ///
+  /// This constructor should be used when its unclear if the [jsObject] has
+  /// already been wrapped.
+  factory WindowImpl.safeFromJsObject(JsObject jsObject) =>
+      (jsObject.dartObject ?? WindowImpl.fromJsObject(jsObject)) as WindowImpl;
+
+  @override
+  CustomElementRegistry get customElements =>
+      throw UnimplementedError('customElements not implemented');
+
+  @override
+  Document get document => throw UnimplementedError('document not implemented');
+}
+
+/// The [Window] object.
+Window get window => throw UnimplementedError('window not implemented');
