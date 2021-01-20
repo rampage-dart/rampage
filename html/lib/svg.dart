547f400b55a66ed624968915689019622f0563da
Don <don.j.olmstead@gmail.com>
2021-01-19 18:23:30 -0800

    feat(svg): Add SvgElement definition
    Create a stub for the browser implementation and its IDL file.

---
+++ html/lib/svg.dart
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+export 'src/svg_element.dart';
