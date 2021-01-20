8847bfdc4bea78a0cd985ae9ede6e610e210c9f0
Don <don.j.olmstead@gmail.com>
2021-01-19 18:23:30 -0800

    feat(svg): Add SvgElement definition
    Create a stub for the browser implementation and its IDL file.

---
+++ html/lib/browser/src/svg_element.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+import 'dart:js';
+
+import 'package:rampage_html/svg.dart';
+
+import 'element.dart';
+import 'global_event_handlers.dart';
+import 'html_or_svg_element.dart';
+
+/// Browser implementation of [SvgElement].
+class SvgElementImpl extends ElementImpl
+    with HtmlOrSvgElementImpl, GlobalEventHandlersImpl
+    implements SvgElement {
+  /// Creates an instance of [SvgElementImpl] from the [jsObject].
+  SvgElementImpl.fromJsObject(JsObject jsObject) : super.fromJsObject(jsObject);
+}
