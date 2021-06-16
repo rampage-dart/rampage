458f31fff9a85b3cbbcf4a204eb3b9e3c53c0a29
Don <don.j.olmstead@gmail.com>
2021-06-15 20:03:28 -0700

    feat() Add NonElementParentNodeJsObject

---
+++ html/lib/browser/src/js/non_element_parent_node.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:js';
+
+extension NonElementParentNodeJsObject on JsObject {
+  Object? getElementById(String id) =>
+      callMethod('getElementById', <Object?>[id]);
+}
