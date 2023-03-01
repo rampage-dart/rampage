76d6314356fd5f27eb4242d328de6b6c00df76f7
Don Olmstead <don.j.olmstead@gmail.com>
2023-03-01 13:35:55 -0800

    fix(): Remove unused imports
    Runs fix for `unused_import`.

--- html/lib/browser/src/node.dart
+++ html/lib/browser/src/node.dart
@@ -3,8 +3,6 @@
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
-import 'dart:js';
-
 import 'package:rampage_html/html.dart';
 
 import 'element_from_js_object.dart';
