1acfd3380152b173ad615d62991fec9793e3c9b4
Don <don.j.olmstead@gmail.com>
2025-02-03 15:20:54 -0800

    chore(): reboot package

--- html/lib/src/document_or_shadow_root.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 the Rampage Project Authors.
-// Please see the AUTHORS file for details. All rights reserved.
-// Use of this source code is governed by a zlib license that can be found in
-// the LICENSE file.
-
-import 'node.dart';
-
-/// Provides APIs that are shared between [Document]s and [ShadowRoot]s.
-abstract class DocumentOrShadowRoot implements Node {}
